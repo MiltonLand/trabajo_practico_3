02c2cac143f87b9a6bc90e0c3a7e20266e6668e8
alesfranco <34348263+alesfranco@users.noreply.github.com>
2017-12-09 19:29:34 -0300

    Add files via upload

---
+++ Tp3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tp3.MvcApplication" Language="C#" %>
---
+++ Tp3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tp3.Startup))]
+namespace Tp3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
