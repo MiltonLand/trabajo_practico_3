7d9da0ce3d215c8ba2b618050476add0ed7d207c
alesfranco <34348263+alesfranco@users.noreply.github.com>
2017-12-09 19:32:25 -0300

    Add files via upload

---
+++ Tp3/Controllers/EmployeeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataAccess;
+using Services;
+
+namespace Tp3.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View(EmployeeService.GetAll());
+        }
+    }
+}
