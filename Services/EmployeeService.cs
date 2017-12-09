7859b4282101437f56548cbe93cf1a5f71e1da2e
alesfranco <34348263+alesfranco@users.noreply.github.com>
2017-12-09 19:23:04 -0300

    Add files via upload
    Services

---
+++ Services/EmployeeService.cs
@@ -0,0 +1,28 @@
+﻿using DataAccess;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public static class EmployeeService
+    {
+        public static List<Employees> GetAll() {
+            var ER = new EmployeeRepository();
+              var list= ER.GetEmployeesInclude();
+            return list;
+        }
+
+
+        public static Employees FindID(int? id)
+        {
+            var ER = new EmployeeRepository();
+            return ER.FindID(id);
+           
+        }
+
+    }
+}
