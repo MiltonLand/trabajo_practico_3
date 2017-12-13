304eacb85c1fbbc4739e430c3bda83431172ef7e
Sebastián Flores <ForeverZeroDragon@Gmail.com>
2017-12-13 08:18:52 -0300

    ADD CRUD Countries

--- Services/CountryService.cs
+++ Services/CountryService.cs
@@ -25,5 +25,61 @@ namespace Services
                 CountryName = c.CountryName
             }).ToList();
         }
+
+        public CountryDto Find(int id)
+        {
+            var country = _countryRepository.Set().FirstOrDefault(c => c.CountryID == id);
+
+            var newCountry = new CountryDto
+            {
+                CountryID = country.CountryID,
+                CountryName = country.CountryName
+            };
+
+            return newCountry;
+        }
+
+        public void Create(CountryDto country)
+        {
+            var newCountry = new Country
+            {
+                CountryID = country.CountryID,
+                CountryName = country.CountryName
+            };
+
+            _countryRepository.Persist(newCountry);
+            _countryRepository.SaveChanges();
+        }
+
+        public void Create(int countryId, string name)
+        {
+            var newCountry = new Country();
+
+            newCountry.CountryID = countryId;
+            newCountry.CountryName = name;
+
+            _countryRepository.Persist(newCountry);
+            _countryRepository.SaveChanges();
+        }
+
+
+        public void Update(CountryDto country)
+        {
+            var newCountry = _countryRepository.Set().FirstOrDefault(c => c.CountryID == country.CountryID);
+
+            newCountry.CountryID = country.CountryID;
+            newCountry.CountryName = country.CountryName;
+
+            _countryRepository.SaveChanges();
+        }
+
+        public void Delete(CountryDto country)
+        {
+            var newCountry = _countryRepository.Set().FirstOrDefault(c => c.CountryID == country.CountryID);
+
+            _countryRepository.Remove(newCountry);
+            _countryRepository.SaveChanges();
+
+        }
     }
 }
